073b1b6fefa2eeff3941227558e255a927d6479c
Anim Malvat <malvatanim@gmail.com>
2021-12-12 17:56:41 -0500

    init: changing db to postgresql

--- EmbraceTheAnimal/settings.py
+++ EmbraceTheAnimal/settings.py
@@ -75,10 +75,21 @@ django_heroku.settings(locals())
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'embrace-the-animal', 
+        'USER': 'postgres',
+        'PASSWORD': 'anim1610',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
