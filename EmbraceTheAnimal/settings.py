de2e181378e3f70ca357cc6937d528e72b342980
Anim Malvat <malvatanim@gmail.com>
2021-12-12 16:37:31 -0500

    init: adding heroku configuration

--- EmbraceTheAnimal/settings.py
+++ EmbraceTheAnimal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -69,6 +69,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EmbraceTheAnimal.wsgi.application'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
